be01b87a2e0086ae3c202ad0e8e3bfad8e4c206d
Vincent Pauley <vincent@localhost.localdomain>
2018-12-13 21:01:14 -0600

    axios setup and dummy note submit

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ require( 'dotenv' ).config();
 
 const express = require( 'express' );
 const app = express();
+const bodyParser = require( 'body-parser' );
 
 app.use( express.static( 'dist' ) );
+app.use( bodyParser.json() );
+
+app.post( '/note', ( req, res ) => {
+
+    console.log( req.body );
+    
+    res.status( 200 );
+    res.send({ result: 'received' });
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container">
     <h1>{{ header }}</h1>
-
     <note-input></note-input>
   </div>
 </template>
 
 <script>
+
+  import axios from 'axios';
 
   import { EventBus } from './event-bus.js';
 
@@ -18,13 +19,39 @@
               header: 'Note Keeper'
           };
       },
+      methods: {
+          /**
+           * @function add_new_note
+           * 
+           * Async method that posts contents of a "Note" to the
+           * server for saving.
+           * 
+           * @param {object} note - contains title and body of note
+           */
+          add_new_note: async function( note ) {
+          
+                let note_result = await axios.post( '/note', { title: note.title, body: note.body });
+
+
+                if( typeof note_result === 'object' && note_result.status === 200 ) {
+
+                    console.log( note_result.data );
+
+                } else {
+
+                    alert( 'POST failed to submit note' );
+                }
+
+          }
+      },
       components: { NoteInput },
       created() {
 
+          let root = this;
+
           EventBus.$on( 'new-note', noteDetails => {
 
-              console.log( 'New Note in Root' );
-              console.log( noteDetails );
+              root.add_new_note( noteDetails );
           });
       }
   }
